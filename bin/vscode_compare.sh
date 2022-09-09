8654c67183b9ff24d476689c29ca23acfce47c96
Logan Mzz <loganmzz@users.noreply.github.com>
2022-09-09 11:38:43 +0200

    VS Code (Update)

--- bin/vscode_compare.sh
+++ bin/vscode_compare.sh
@@ -3,9 +3,22 @@
 basedir="$(cd "$(dirname "${BASH_SOURCE}")/.." >/dev/null; pwd)" || exit 1
 
 SRC_PATH=~/.config/Code/User
-TGT_PATH="${basedir}/files/vscode"
+TGT_PATH="${basedir}/files/vscode/config"
 
 cd "${TGT_PATH}"
 for file in $(find . -type f); do
+    existing_backups="$(\ls -x "${SRC_PATH}/${file}".* 2>/dev/null | xargs realpath)"
+    [[ -z "${existing_backups}" ]] || {
+        echo "------------------------------------------------"
+        echo "Found backups:"
+        echo "${existing_backups}"
+    }
     code --diff "${SRC_PATH}/${file}" "${TGT_PATH}/${file}"
+done
+
+cd "${SRC_PATH}/snippets"
+for file in $(find . -name '*.json'); do
+    [[ -f "${TGT_PATH}/snippets/${file}" ]] || {
+        echo "No sources for '$(realpath "${SRC_PATH}/snippets/${file}")'"
+    }
 done
