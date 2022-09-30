2f7a721cd3a5a2b46d56682a2880672ff9a7566f
Logan Mzz <loganmzz@users.noreply.github.com>
2022-09-30 23:29:03 +0200

    Let's passed arguments to `ansible-playbook`

--- main.sh
+++ main.sh
@@ -3,4 +3,4 @@
 basedir="$(cd "$(dirname "${BASH_SOURCE}")" >/dev/null; pwd)" || exit 1
 
 cd "${basedir}"
-ansible-playbook main.yaml --ask-become-pass
+ansible-playbook main.yaml --ask-become-pass "$@"
