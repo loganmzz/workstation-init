f23c9890f6f744afd28e54fc1970f664098e2c42
Logan Mzz <loganmzz@users.noreply.github.com>
2022-11-10 08:56:21 +0100

    Adds task selection

--- main.sh
+++ main.sh
@@ -3,4 +3,8 @@
 basedir="$(cd "$(dirname "${BASH_SOURCE}")" >/dev/null; pwd)" || exit 1
 
 cd "${basedir}"
-ansible-playbook main.yaml --ask-become-pass "$@"
+args=(--ask-become-pass)
+[[ $# == 0 ]] || {
+    args+=(--extra-vars "$(jq '{enabled_tasks:$ARGS.positional}' --args "$@" -c -n)")
+}
+ansible-playbook main.yaml "${args[@]}"
